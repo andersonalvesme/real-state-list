4ef498b7516b0a9af14257c08ab0d821f4dd1028
Anderson Alves <andersonalves.me@gmail.com>
2024-01-22 22:33:40 -0300

    adjust size of container for big screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1240px',
+      }
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
