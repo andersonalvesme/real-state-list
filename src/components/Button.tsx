5001bf1a7e59bc7ed891ac830cf720cbb8c1524e
Anderson Alves <andersonalves.me@gmail.com>
2024-01-27 17:11:43 -0300

    implement button sizes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,21 @@
 import { MouseEventHandler } from "react";
+import cn from "classnames";
 
 type ButtonType = {
   label: string;
+  size?: string;
   onClick?: MouseEventHandler;
 }
 
-export default function Button({ label, onClick }: ButtonType) {
+export default function Button({ label, size = 'sm', onClick }: ButtonType) {
   return (
-    <button className="bg-blue-500 text-white p-1 w-full rounded" onClick={onClick}>
+    <button
+      className={cn("bg-blue-500 text-white w-full rounded hover:bg-blue-600", {
+        "p-1": size === 'sm',
+        "p-2": size === 'md'
+      })}
+      onClick={onClick}
+    >
       {label}
     </button>
   )
