2afa4d753130e7d3374698d3f287ff6686a1388e
Anderson Alves <andersonalves.me@gmail.com>
2024-01-27 17:11:18 -0300

    adjust mocked data to load

--- src/assets/dataMocked.js
+++ src/assets/dataMocked.js
@@ -1,3 +1,24 @@
+import house1Thumb from './images/house1-thumb.jpg'
+import house1Picture from './images/house1-w1024_h768_x2.jpg'
+import house2Thumb from './images/house2-thumb.jpg'
+import house2Picture from './images/house2-w1024_h768_x2.jpg'
+import house3Thumb from './images/house3-thumb.jpg'
+import house3Picture from './images/house3-w1024_h768_x2.jpg'
+import house4Thumb from './images/house4-thumb.jpg'
+import house4Picture from './images/house4-w1024_h768_x2.jpg'
+import house5Thumb from './images/house5-thumb.jpg'
+import house5Picture from './images/house5-w1024_h768_x2.jpg'
+import house6Thumb from './images/house6-thumb.jpg'
+import house6Picture from './images/house6-w1024_h768_x2.jpg'
+import house7Thumb from './images/house7-thumb.jpg'
+import house7Picture from './images/house7-w1024_h768_x2.jpg'
+import house8Thumb from './images/house8-thumb.jpg'
+import house8Picture from './images/house8-w1024_h768_x2.jpg'
+import house9Thumb from './images/house9-thumb.jpg'
+import house9Picture from './images/house9-w1024_h768_x2.jpg'
+import house10Thumb from './images/house10-thumb.jpg'
+import house10Picture from './images/house10-w1024_h768_x2.jpg'
+
 export const dataMocked = [
   {
     "Id": 2534,
@@ -5,8 +26,8 @@ export const dataMocked = [
     "Title": "Lovely House with Mountain View",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 349000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house1Thumb.src,
+    "PictureURL": house1Picture.src,
     "Location": "Paris",
     "Sqft": 4638,
     "Bedrooms": 4,
@@ -20,8 +41,8 @@ export const dataMocked = [
     "Title": "Single Family House",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 499000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house2Thumb.src,
+    "PictureURL": house2Picture.src,
     "Location": "Barcelona",
     "Sqft": 5638,
     "Bedrooms": 5,
@@ -35,8 +56,8 @@ export const dataMocked = [
     "Title": "5 bedroom house in Sao Paulo",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 449000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house3Thumb.src,
+    "PictureURL": house3Picture.src,
     "Location": "Sao Paulo",
     "Sqft": 5838,
     "Bedrooms": 5,
@@ -50,8 +71,8 @@ export const dataMocked = [
     "Title": "3 bedroom house in Cartagena",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 350000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house4Thumb.src,
+    "PictureURL": house4Picture.src,
     "Location": "Cartagena",
     "Sqft": 2638,
     "Bedrooms": 3,
@@ -65,8 +86,8 @@ export const dataMocked = [
     "Title": "Beautiful traditional ranch in California",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 575000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house5Thumb.src,
+    "PictureURL": house5Picture.src,
     "Location": "California",
     "Sqft": 4638,
     "Bedrooms": 4,
@@ -80,8 +101,8 @@ export const dataMocked = [
     "Title": "4 bedrooms with a spacious garage",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 475000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house6Thumb.src,
+    "PictureURL": house6Picture.src,
     "Location": "Ontario",
     "Sqft": 2048,
     "Bedrooms": 4,
@@ -95,8 +116,8 @@ export const dataMocked = [
     "Title": "Beautiful spacious 1306 sq ft Bungalow",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 374000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house7Thumb.src,
+    "PictureURL": house7Picture.src,
     "Location": "Alberta",
     "Sqft": 1306,
     "Bedrooms": 5,
@@ -110,8 +131,8 @@ export const dataMocked = [
     "Title": "EXQUISITE POND VIEW HOME",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 450000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house8Thumb.src,
+    "PictureURL": house8Picture.src,
     "Location": "Paris",
     "Sqft": 1935,
     "Bedrooms": 3,
@@ -125,8 +146,8 @@ export const dataMocked = [
     "Title": "Meticulously maintained Lampman Family Home",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 199000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house9Thumb.src,
+    "PictureURL": house9Picture.src,
     "Location": "Saskatchewan",
     "Sqft": 1204,
     "Bedrooms": 5,
@@ -140,8 +161,8 @@ export const dataMocked = [
     "Title": "Charming updated gem",
     "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum convallis vehicula. Morbi ac gravida mi. Nullam aliquam ut lorem ut fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur consequat magna risus, at tempus magna semper eget. Sed eget finibus nisl, ut pellentesque mi. Pellentesque vulputate ultricies posuere. Vestibulum sagittis at eros non accumsan. Proin nec sollicitudin ante, tempus dignissim velit. Quisque bibendum pharetra purus, in cursus tortor condimentum et. Etiam vel dictum lacus. Nulla non ligula at tortor cursus sollicitudin blandit ut sem.",
     "Sale Price": 725000,
-    "ThumbnailURL": "https://dummyimage.com/150x150/AAF4EE/000000",
-    "PictureURL": "https://dummyimage.com/350x350/AAF4EE/000000",
+    "ThumbnailURL": house10Thumb.src,
+    "PictureURL": house10Picture.src,
     "Location": "Seattle",
     "Sqft": 6194,
     "Bedrooms": 4,
