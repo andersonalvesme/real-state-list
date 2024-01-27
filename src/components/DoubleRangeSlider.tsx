13608b3ae63dd1470ee529d573edf5089c9d2239
Anderson Alves <andersonalves.me@gmail.com>
2024-01-27 18:02:36 -0300

    adjust component slider to set filters

--- src/components/DoubleRangeSlider.tsx
+++ src/components/DoubleRangeSlider.tsx
@@ -1,31 +1,32 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import cn from "classnames";
 import './DoubleRangeSlider.css'
+import { useDebouncedCallback } from "use-debounce";
+import { PropertyAttributesType } from "@/app/properties/search";
 
 type DoubleRangeSliderType = {
   min: number,
   max: number,
-  onChange: Function
+  onChange: Function,
+  filters: PropertyAttributesType
 };
 
-export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSliderType) {
-  const [minValue, setMinValue] = useState(min);
-  const [maxValue, setMaxValue] = useState(max);
+export default function DoubleRangeSlider({ min, max, onChange, filters }: DoubleRangeSliderType) {
+  const [minValue, setMinValue] = useState(filters.range_start ?? min);
+  const [maxValue, setMaxValue] = useState(filters.range_end ?? max);
   const minValueRef = useRef<HTMLInputElement>(null);
   const maxValueRef = useRef<HTMLInputElement>(null);
   const range = useRef<HTMLInputElement>(null);
 
-  // Convert to percentage
   const getPercent = useCallback(
     (value: number) => Math.round(((value - min) / (max - min)) * 100),
     [min, max]
   );
 
-  // Set width of the range to decrease from the left side
   useEffect(() => {
     if (maxValueRef.current) {
       const minPercent = getPercent(minValue);
-      const maxPercent = getPercent(+maxValueRef.current.value); // Preceding with '+' converts the value from type string to type number
+      const maxPercent = getPercent(+maxValueRef.current.value);
 
       if (range.current) {
         range.current.style.left = `${minPercent}%`;
@@ -34,7 +35,6 @@ export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSli
     }
   }, [minValue, getPercent]);
 
-  // Set width of the range to decrease from the right side
   useEffect(() => {
     if (minValueRef.current) {
       const minPercent = getPercent(+minValueRef.current.value);
@@ -46,15 +46,15 @@ export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSli
     }
   }, [maxValue, getPercent]);
 
-  // Get min and max values when their state changes
-  useEffect(() => {
-    onChange({ min: minValue, max: maxValue });
-  }, [minValue, maxValue, onChange]);
+  const setAfterDebounce = useDebouncedCallback((event, value) => {
+    onChange(event);
+  }, 500);
 
   return (
     <div className="m-3">
       <input
         type="range"
+        name="range_start"
         min={min}
         max={max}
         value={minValue}
@@ -63,6 +63,7 @@ export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSli
           const value = Math.min(+event.target.value, maxValue - 1);
           setMinValue(value);
           event.target.value = value.toString();
+          setAfterDebounce(event, value)
         }}
         className={cn("thumb thumb-zindex-3", {
           "thumb-zindex-5": minValue > max - 100
@@ -70,6 +71,7 @@ export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSli
       />
       <input
         type="range"
+        name="range_end"
         min={min}
         max={max}
         value={maxValue}
@@ -78,6 +80,7 @@ export default function DoubleRangeSlider({ min, max, onChange }: DoubleRangeSli
           const value = Math.max(+event.target.value, minValue + 1);
           setMaxValue(value);
           event.target.value = value.toString();
+          setAfterDebounce(event, value)
         }}
         className="thumb thumb-zindex-4"
       />
